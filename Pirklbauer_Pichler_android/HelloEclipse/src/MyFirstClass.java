69f1d4a137695bab47b4ec998c8039a788905d00
pirklbauer31 <pirklbauer31@gmail.com>
2016-03-10 17:16:34 +0100

    MyFirstClas.java changed!

--- Pirklbauer_Pichler_android/HelloEclipse/src/MyFirstClass.java
+++ Pirklbauer_Pichler_android/HelloEclipse/src/MyFirstClass.java
@@ -2,6 +2,7 @@
 public class MyFirstClass {
 	public static void main(String[] args) {
 		System.out.println("Hello Eclipse!");
+		Out.println("Hello GITHUB!");
 		Out.open("MyFile.txt");
 		Out.println("Hello Eclipse File!");
 		Out.close();
